9e201042331dbe5528b9ddd81a9bc0301858d936
trollingchar <rodind-1@mail.ru>
2017-04-05 00:45:20 +0400

    new client now can connect to new server

--- frontend/Assets/code/net/WSConnection.cs
+++ frontend/Assets/code/net/WSConnection.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Crutch : MonoBehaviour {
-
+public class WSConnection : MonoBehaviour {
     WebSocket socket;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+    void Start () { }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update () {
+        if (socket == null) return;
+        for (byte[] bytes = socket.Recv();
+            bytes != null;
+            bytes = socket.Recv())
+        {
+            Debug.Log(BitConverter.ToString(bytes));
+        }
+    }
+
+    public void Authorize (string s, int id) {
+        Debug.Log("kek");
+        socket = new WebSocket(new Uri("ws://localhost:8080/events/"));
+        StartCoroutine(socket.Connect());
+    }
 }
