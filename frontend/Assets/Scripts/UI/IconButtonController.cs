34f03f8dad8bf3b2c6ba3df89c967fd3a80e29d0
trollingchar <dmitriyrodin0@gmail.com>
2017-07-31 22:33:21 +0300

    added gitignore for unity

---
+++ frontend/Assets/Scripts/UI/IconButtonController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace UI {
+
+    public class IconButtonController : MonoBehaviour {
+
+        [SerializeField] private Text _text;
+        [SerializeField] private Image _icon;
+
+    }
+
+}
