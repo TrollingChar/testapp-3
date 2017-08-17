019067aba79c260ed85fb8e5c211be4c7d61e1b2
trollingchar <dmitriyrodin0@gmail.com>
2017-08-18 00:38:20 +0300

    scaling fixed

--- frontend/Assets/Scripts/UI/Panels/ArsenalPanel.cs
+++ frontend/Assets/Scripts/UI/Panels/ArsenalPanel.cs
@@ -8,10 +8,12 @@ namespace UI.Panels {
     public class ArsenalPanel : MonoBehaviour {
 
         [SerializeField] private GameObject _weaponButton;
+        private int _i = 0;
 
         private void Start () {
             AddWeapon<BazookaWeapon>();
-            AddEmpty(11);
+            AddWeapon<GrenadeWeapon>();
+            AddEmpty(6);
         }
 
 
@@ -19,18 +21,20 @@ namespace UI.Panels {
             var button = Instantiate(_weaponButton);
             var component = _weaponButton.GetComponent<WeaponButton>();
             component.SetAmmo(1);
-            button.transform.SetParent(gameObject.transform);
+            button.transform.SetParent(gameObject.transform, false);
+            button.name = "button " + _i++;
             button.transform.SetAsLastSibling();
 //            button.transform.SetAsFirstSibling();
         }
 
 
-        private void AddEmpty (int count) {
+        private void AddEmpty (int count = 1) {
             for (int i = 0; i < count; i++) {
                 var button = Instantiate(_weaponButton);
                 var component = _weaponButton.GetComponent<WeaponButton>();
                 component.SetAmmo(0);
-                button.transform.SetParent(gameObject.transform);
+                button.transform.SetParent(gameObject.transform, false);
+                button.name = "button " + _i++;
                 button.transform.SetAsLastSibling();
 //                button.transform.SetAsFirstSibling();
             }
