5f29fa5896514870ae4d258941f24bb92fada452
Dmitriy Rodin <dmitriy.rodin@tortugasocial.com>
2017-10-30 15:21:35 +0300

    stub for phantom grenade

---
+++ frontend/Assets/Scripts/Battle/Weapons/WeaponTypes/Thrown/PhantomGrenade.cs
@@ -0,0 +1,31 @@
+﻿using Assets;
+using Attributes;
+using Utils.Singleton;
+
+namespace Battle.Weapons.WeaponTypes.Thrown
+{
+    public class PhantomGrenade
+    {
+        [Weapon(WeaponId.PhantomGrenade)]
+        public class PhantomGrenadeWeapon : StandardWeapon {
+
+            public static WeaponDescriptor Descriptor {
+                get {
+                    return new WeaponDescriptor(
+                        WeaponId.PhantomGrenade,
+                        The<WeaponIcons>.Get().PhantomGrenade
+                    );
+                }
+            }
+
+
+            protected override void OnEquip () {
+//            CrossHair = new LineCrosshair();
+            }
+
+
+            protected override void OnShoot () {}
+
+        }
+    }
+}
