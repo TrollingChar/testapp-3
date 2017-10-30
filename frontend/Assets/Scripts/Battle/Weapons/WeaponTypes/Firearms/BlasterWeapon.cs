d54bf38ccf61f57e4fe291d21dde6b72beccb156
Dmitriy Rodin <dmitriy.rodin@tortugasocial.com>
2017-10-30 12:48:05 +0300

    stubs for firearms

---
+++ frontend/Assets/Scripts/Battle/Weapons/WeaponTypes/Firearms/BlasterWeapon.cs
@@ -0,0 +1,37 @@
+﻿using Assets;
+using Attributes;
+using Battle.Weapons.Crosshairs;
+using Utils.Singleton;
+
+
+namespace Battle.Weapons.WeaponTypes.Firearms {
+
+    [Weapon(WeaponId.Blaster)]
+    public class BlasterWeapon : StandardWeapon {
+
+        public static WeaponDescriptor Descriptor {
+            get {
+                return new WeaponDescriptor(
+                    WeaponId.Blaster,
+                    The<WeaponIcons>.Get().Blaster
+                );
+            }
+        }
+
+
+        protected override void OnEquip () {
+            Attacks = 2;
+//            CrossHair = new LineCrosshair();
+        }
+
+
+        protected override void OnBeginAttack () {
+            UseAmmo();
+        }
+
+
+        protected override void OnShoot () {}
+
+    }
+
+}
---
+++ frontend/Assets/Scripts/Battle/Weapons/WeaponTypes/Firearms/GsomRaycasterWeapon.cs
@@ -0,0 +1,37 @@
+﻿using Assets;
+using Attributes;
+using Battle.Weapons.Crosshairs;
+using Utils.Singleton;
+
+
+namespace Battle.Weapons.WeaponTypes.Firearms {
+
+    [Weapon(WeaponId.GsomRaycaster)]
+    public class GsomRaycasterWeapon : StandardWeapon {
+
+        public static WeaponDescriptor Descriptor {
+            get {
+                return new WeaponDescriptor(
+                    WeaponId.GsomRaycaster,
+                    The<WeaponIcons>.Get().GsomRaycaster
+                );
+            }
+        }
+
+
+        protected override void OnEquip () {
+            Shots = 10;
+//            CrossHair = new LineCrosshair();
+        }
+
+
+        protected override void OnBeginAttack () {
+            UseAmmo();
+        }
+
+
+        protected override void OnShoot () {}
+
+    }
+
+}
