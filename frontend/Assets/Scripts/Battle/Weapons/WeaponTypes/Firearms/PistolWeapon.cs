62b42f57068ed63a85f99f1b14d8c520fac47d89
Dmitriy Rodin <dmitriy.rodin@tortugasocial.com>
2017-10-31 11:21:58 +0300

    pistol w/o sprite and bullets

--- frontend/Assets/Scripts/Battle/Weapons/WeaponTypes/Firearms/PistolWeapon.cs
+++ frontend/Assets/Scripts/Battle/Weapons/WeaponTypes/Firearms/PistolWeapon.cs
@@ -2,12 +2,15 @@
 using Attributes;
 using Battle.Weapons.Crosshairs;
 using Core;
+using UnityEngine;
 
 
 namespace Battle.Weapons.WeaponTypes.Firearms {
 
     [Weapon(WeaponId.Pistol)]
     public class PistolWeapon : StandardWeapon {
+        
+        private LineCrosshair _crosshair;
 
         public static WeaponDescriptor Descriptor {
             get {
@@ -21,7 +24,17 @@ namespace Battle.Weapons.WeaponTypes.Firearms {
 
         protected override void OnEquip () {
             Shots = 5;
-//            CrossHair = new LineCrosshair();
+            ShotCooldown = 30;
+
+            var battleAssets = The.BattleAssets;
+            
+            _crosshair = UnityEngine.Object.Instantiate(
+                battleAssets.LineCrosshair,
+                GameObject.transform,
+                false
+            ).GetComponent<LineCrosshair>();
+            
+            // todo: sprite
         }
 
 
@@ -30,7 +43,19 @@ namespace Battle.Weapons.WeaponTypes.Firearms {
         }
 
 
-        protected override void OnShoot () {}
+        protected override void OnShoot() {
+            // todo
+            // создать снаряд и сразу же вызвать у него Update
+            // или написать функцию World.CastRay
+            // второй вариант предпочтительнее так как есть еще ультравинтовка лучи которой проходят через все
+            Debug.Log("bang");
+        }
+
+
+        protected override void OnUpdate () {
+            UpdateLineCrosshair(_crosshair);
+//            UpdateAimedWeapon(_sprite);
+        }
 
     }
 
