5e69d0204fa4db9d946fd0a0064afed2b6655de3
trollingchar <dmitriyrodin0@gmail.com>
2017-11-17 22:58:41 +0300

    new geom class

--- frontend/Assets/Scripts/Geometry/Physics.cs
+++ frontend/Assets/Scripts/Geometry/Physics.cs
@@ -1,64 +1,58 @@
 ﻿using Battle.Physics.Collisions;
+using UnityEngine;
+using BoxCollider = Battle.Physics.Collisions.BoxCollider;
+using Collision = Battle.Physics.Collisions.Collision;
 
-namespace Geometry
-{
-    public class Physics
-    {
-        public static bool Overlap(CircleCollider a, CircleCollider b)
-        {
+
+namespace Geometry {
+
+    public static class Physics {
+
+        public static bool Overlap (CircleCollider a, CircleCollider b) {
             float rr = a.Radius + b.Radius;
             return XY.SqrDistance(a.Center, b.Center) < rr * rr;
         }
 
-        public static bool Overlap(CircleCollider c, BoxCollider b)
-        {
-            XY o = c.Center;
+
+        public static bool Overlap (CircleCollider c, BoxCollider b) {
+            //* switch algorithm
+            float closestX = Mathf.Clamp(c.Center.X, b.Left, b.Right);
+            float closestY = Mathf.Clamp(c.Center.Y, b.Bottom, b.Top);
+
+            float dx = c.Center.X - closestX;
+            float dy = c.Center.Y - closestY;
+
+            return dx * dx + dy * dy < c.Radius * c.Radius;
+            /*/ else
+            float halfW = 0.5f * (b.Right - b.Left);
+            float halfH = 0.5f * (b.Top - b.Bottom);
+
+            float distX = Mathf.Abs(c.Center.X - b.Left - halfW);
+            float distY = Mathf.Abs(c.Center.Y - b.Bottom - halfH);
+
             float r = c.Radius;
-            float top = b.Top;
-            float left = b.Left;
-            float right = b.Right;
-            float bottom = b.Bottom;
+            if (distX >= halfW + r || distY >= halfH + r) return false;
+            if (distX < halfW || distY < halfH) return true;
 
-            float r2 = r * r;
-            
-            if (left >= o.X + r || right <= o.X - r || bottom >= o.Y + r || top <= o.Y - r) return false;
-
-            if (o.X >= right) {
-                if (o.Y >= top)    return XY.SqrDistance(o, new XY(right, top)) < r2;
-                if (o.Y <= bottom) return XY.SqrDistance(o, new XY(right, bottom)) < r2;
-                return true;
-            }
-            if (o.X <= left) {
-                if (o.Y >= top)    return XY.SqrDistance(o, new XY(left, top)) < r2;
-                if (o.Y <= bottom) return XY.SqrDistance(o, new XY(left, bottom)) < r2;
-                return true;
-            }
-            return true;
+            distX -= halfW;
+            distY -= halfH;
+            return distX * distX + distY * distY < r * r;
+            //*/
         }
 
-        public static bool Overlap(BoxCollider a, BoxCollider b)
-        {
+
+        public static bool Overlap (BoxCollider a, BoxCollider b) {
             return a.Top > b.Bottom
                 && a.Left < b.Right
                 && a.Right > b.Left
                 && a.Bottom < b.Top;
         }
 
-        public static Collision FlyInto(CircleCollider a, CircleCollider b, XY v)
-        {
-            if (Overlap(a, b)) return null;
-            var rayDist = Geom.RayToCircle(a.Center, v, b.Center, a.Radius + b.Radius);
-            // todo: численными методами подобрать rayDist чтобы третий закон выполнялся
-        }
 
-        public static Collision FlyInto(CircleCollider c, BoxCollider b, XY v)
-        {
-            
-        }
+        public static Collision FlyInto (CircleCollider a, CircleCollider b, XY v) {}
+        public static Collision FlyInto (CircleCollider c, BoxCollider b, XY v) {}
+        public static Collision FlyInto (BoxCollider a, BoxCollider b, XY v) {}
 
-        public static Collision FlyInto(BoxCollider a, BoxCollider b, XY v)
-        {
-            
-        }
     }
+
 }
